054c6d94f128419c3c4e97d3ba049bd51626c7c3
MaruGR26 <maruromero.gr@proton.me>
2026-05-06 14:39:37 -0600

    Create verde_chain.ts

---
+++ tests/verde_chain.ts
@@ -0,0 +1,7 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { VerdeChain } from "../target/types/verde_chain";
+
+describe("verde_chain", () => {
+  // Próximamente: Integración de pruebas automatizadas para PLD y NDVI
+});
